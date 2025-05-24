163f58654a066f82034a2568ccbb31e41540605e
Matej Josifov <62450091+JosifovMatej404@users.noreply.github.com>
2025-05-24 19:01:19 +0200

    changed urls and settings

--- DjangoGuns/settings.py
+++ DjangoGuns/settings.py
@@ -15,11 +15,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / "secret_key.env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'GunsApp.apps.GunsappConfig',
 ]
 
 MIDDLEWARE = [
